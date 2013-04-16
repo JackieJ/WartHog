21545d9076fb5f2e550dcf8e6257244fd2c30db6
Jennifer H <jennhuang8@gmail.com>
2013-04-15 19:10:28 -0700

    [jennifer] delete unnecessady compass pkg

--- compass_pkg/build/devel/setup.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# generated from catkin/cmake/templates/setup.bash.in
-
-CATKIN_SHELL=bash
-. "/home/robo/Projects/WartHog/compass_pkg/build/devel/setup.sh"
--- compass_pkg/build/devel/setup.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-# generated from catkin/cmake/templates/setup.zsh.in
-
-CATKIN_SHELL=zsh
-emulate sh # emulate POSIX
-. "/home/robo/Projects/WartHog/compass_pkg/build/devel/setup.sh"
-emulate zsh # back to zsh mode
