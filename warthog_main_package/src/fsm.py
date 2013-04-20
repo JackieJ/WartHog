7204cddb1a06feaf91166ef1b87f3c8540ed7c01
Jackie Jin <liveaeon@gmail.com>
2013-04-20 02:03:17 -0700

    [Jackie] Gain tweak on motor and fsm

--- warthog_main_package/src/fsm.py
+++ warthog_main_package/src/fsm.py
@@ -35,11 +35,25 @@ class FSM():
         #self.waypoints = yaml.load(file('/home/robo/Projects/WartHog/warthog_main_package/src/waypoints.yaml','r'))
         self.waypoints = yaml.load(open('/home/robo/Projects/WartHog/warthog_main_package/src/waypoints.yaml', 'r'))
         #retrieve target coordinates from the beginning of the list
-        self.targetCoord = waypoints.pop(0)
-        self.targetPose = self.targetCoord
-        #Constants
+        self.targetPose = self.waypoints.pop(0)
+        #Constants. TWEAKING NEEDED
         self.MAXLINEAR = 3
         self.MAXANGULAR = 3
+        #for generic walking vel
+        self.linearGain = 0.2
+        self.angularGain = 0.2
+        #for full turning in walking mode
+        self.fullTurnAngularGain = 0.2
+        #for vel in camera mode
+        self.cvLinearGain = 0.2
+        self.cvAngularGain = 0.2
+        #for vel when backing up
+        self.backLinearGain = 0.2
+        self.backAngularGain = 0.2
+        #distance bound before entering the camera mode
+        self.targetReachDistanceBound = 3
+        #angle err bound before entering the camera mode
+        self.angleErrBound = 10
         #############for tests#############
         self.testGain = -1
         #self.velocityOutput.linear = self.MAXLINEAR
@@ -58,57 +72,74 @@ class FSM():
             time.sleep(1)
             
     def GPSUTMCallback(self, data):
-        #retrieve gps data first, then grab the gyro data
-        (self.currentPose['utm'])['x'] = data.pose.pose.position.x
-        (self.currentPose['utm'])['y'] = data.pose.pose.position.y
+        #the walking mode has the lowest priority
+        if self.MODE == 0:
+            #retrieve gps data first, then grab the gyro data
+            (self.currentPose['utm'])['x'] = data.pose.pose.position.x
+            (self.currentPose['utm'])['y'] = data.pose.pose.position.y
         
-        #loggings
-        print >> sys.stdout, "target_x", self.targetPose['x']
-        print >> sys.stdout, "target_y", self.targetPose['y']
-        if self.targetPose['hasCone']:
-            print >> sys.stdout, "target has cone: yes"
-        else:
-            print >> sys.stdout, "target has cone: no"
-        print >> sys.stdout, "target direction", self.targetPose['direction']
-        print >> sys.stdout, "current pose_x:", self.currentPose['utm']['x']
-        print >> sys.stdout, "current pose_y:", self.currentPose['utm']['y']    
-        
-        #gyro heading
-        self.gyroSub = rospy.Subscriber("gyro", Float32, self.headingCallback)
-        
+            #loggings
+            print >> sys.stdout, "target_x", self.targetPose['x']
+            print >> sys.stdout, "target_y", self.targetPose['y']
+            if self.targetPose['hasCone']:
+                print >> sys.stdout, "target has cone: yes"
+            else:
+                print >> sys.stdout, "target has cone: no"
+                print >> sys.stdout, "target direction", self.targetPose['direction']
+                print >> sys.stdout, "current pose_x:", self.currentPose['utm']['x']
+                print >> sys.stdout, "current pose_y:", self.currentPose['utm']['y']
+    
+            #gyro heading
+            self.gyroSub = rospy.Subscriber("gyro", Float32, self.headingCallback)
+            
+                
     def cvCallback(self, data):
         if data.data == 'F':#too close
+            self.MODE = 1
             rospy.loginfo("cv:filled")
-            self.linearSpeed = 0
-            self.angularSpeed = 0
+            #target and backup
+            self.backup(self.targetPose['direction'])
+            time.sleep(3)
+            self.targetPose = self.waypoints.pop(0)
+            #back to walk mode
+            self.MODE = 0
         if data.data == 'R':#on the right
+            self.MODE = 1
             rospy.loginfo("cv:on the right")
-            self.linearSpeed = 1
-            self.angularSpeed = -self.MAXANGULAR
+            self.linearSpeed = self.MAXLINEAR * self.cvLinearGain
+            self.angularSpeed = -self.MAXANGULAR * self.cvAngularGain
         if data.data == 'L':#on the left
+            self.MODE = 1
             rospy.loginfo("cv:on the left")
-            self.linearSpeed = 1
-            self.angularSpeed = self.MAXLINEAR
+            self.linearSpeed = self.MAXLINEAR * self.cvLinearGain
+            self.angularSpeed = self.MAXLINEAR * self.cvAngularGain
         if data.data == 'M':#in the center
+            self.MODE = 1
             rospy.loginfo("cv:in the center")
-            self.linearSpeed = 1
+            self.linearSpeed = self.MAXLINEAR * self.cvLinearGain
             self.angularSpeed = 0
         rospy.loginfo("publishing cv")
         self.publish()
         
     def headingCallback(self, data):
-        #heading test
-        print >> sys.stdout, "heading:", data.data
-        #print >> sys.stdout, "float32", type(std_msgs.msg.Float32(0.0))
-        if data.data > 0.0 and data.data <= 180.0:
-            print >> sys.stdout, "turning left"
-            self.velocityOutput.angular = 3
-        elif data.data > 180.0 and data.data <= 359.0:
-            print >> sys.stdout, "turning right"
-            self.velocityOutput.angular = -3
-        print >> sys.stdout, "publishing:", self.velocityOutput.angular    
-        self.motorPublisher.publish(self.velocityOutput)
-
+        # #heading test
+        # print >> sys.stdout, "heading:", data.data
+        # #print >> sys.stdout, "float32", type(std_msgs.msg.Float32(0.0))
+        # if data.data > 0.0 and data.data <= 180.0:
+        #     print >> sys.stdout, "turning left"
+        #     self.velocityOutput.angular = 3
+        # elif data.data > 180.0 and data.data <= 359.0:
+        #     print >> sys.stdout, "turning right"
+        #     self.velocityOutput.angular = -3
+        # print >> sys.stdout, "publishing:", self.velocityOutput.angular    
+        # self.motorPublisher.publish(self.velocityOutput)
+        
+        # retrieve the heading value from gyro and perform calculation
+        self.currentPose['heading'] = data.data
+        #calculate the velocity based on target 
+        self.vCalc()
+        
+        
     def killSwitchHook(self):
         #extra guarantee for kill switch shutdown
         self.velocityOutput.linear = 0
@@ -125,13 +156,66 @@ class FSM():
                         
     def bumperCallback(self, data):
         if data.data:
-            #test
-            self.velocityOutput.linear = -self.velocityOutput.linear
-            #test
-            #if bumper is true, pop out the target coord and go to the next
+            self.MODE = 2
+            errX = self.targetPose['x'] - self.currentPose['utm']['x']
+            errY = self.targetPose['y'] - self.currentPose['utm']['y']
+            distancePow = math.pow(errX, 2) + math.pow(errY, 2)
+            distanceFromTheTarget = math.sqrt(distancePow)
+            if distanceFromTheTarget < 1:
+                self.linearSpeed = 0
+                self.angularSpeed = 0
+                rospy.loginfo("Cone Reached Backing up")
+                self.publish()
+                time.sleep(2)
+                self.backup(self.targetPose['direction'])
+                time.sleep(3)
+                #pop out the target and go to next target
+                self.targetPose = self.waypoints.pop(0)
+                self.MODE = 0
+
+    def backup (self, direction):
+        #backup left
+        if direction == "L":
+            self.linearSpeed = -self.MAXLINEAR * self.backLinearGain
+            self.angularSpeed = -self.MAXANGULAR * self.backAngularGain
+        elif direction == "R":
+            self.linearSpeed = -self.MAXLINEAR * self.backLinearGain
+            self.angularSpeed = self.MAXANGULAR * self.backAngularGain
+        if direction != ' ':
+            self.publish()
             
     def vCalc(self):
-        pass
+        #get err between the current position and the target coordinate
+        errX = self.targetPose['x'] - self.currentPose['utm']['x']
+        errY = self.targetPose['y'] - self.currentPose['utm']['y']
+        
+        #calculate distance between current and target
+        distancePow = math.pow(errX, 2) + math.pow(errY, 2)
+        distanceFromTheTarget = math.sqrt(distancePow)
+        
+        #angle between current and target
+        targetAngle = math.atan2(errY, errX)*(180/math.pi)
+        
+        while targetAngle < 0:
+            targetAngle += 360
+        angleErr = targetAngle - self.currentPose['heading']
+        
+        if (angleErr >= 90 and angleErr <= 180) or angleErr <= -180:
+            self.linearSpeed = 0
+            self.angularSpeed = -self.fullTurnAngularGain * self.MAXANGULAR
+        elif angleErr > -90 and angleErr < 90:
+            self.linearSpeed = math.cos(angleErr) * self.MAXLINEAR * self.linearGain
+            self.angularSpeed = math.sin(angleErr)* self.MAXANGULAR
+            
+        self.publish()
+        
+        if distanceFromTheTarget < self.targetReachDistanceBound and math.fabs(angleErr) <= self.angleErrBound: 
+            #go to detection mode
+            if self.targetPose['hasCone']:
+                self.MODE = 1
+            else:
+                #goto next waypoint
+                self.targetPose = self.waypoints.pop(0)
         
     def publish(self):
         if self.isKilled:
